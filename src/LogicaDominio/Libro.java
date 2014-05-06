83a04bc6183be61c20334c9a8a0668713238b084
fane <a.fanelli88@gmail.com>
2014-05-06 17:23:27 +0200

    Sistemata entity Libro

--- src/LogicaDominio/Libro.java
+++ src/LogicaDominio/Libro.java
@@ -9,8 +9,6 @@ package LogicaDominio;
 import java.io.Serializable;
 import java.util.Collection;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
